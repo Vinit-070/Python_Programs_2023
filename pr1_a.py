5b8d5dc2baef49dcdf941c89d11b5dd1b910bc06
Vinit-070 <100857730+Vinit-070@users.noreply.github.com>
2023-04-26 19:16:53 +0530

    Delete pr1_a.py

--- pr1_a.py
+++
@@ -1,8 +0,0 @@
-# Author : Vinit Patel
-# Aim : Write a program to check whether a passed letter is a vowel or not.
-letter = ['a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U']
-alpha = input("Enter the letter : ")
-if alpha in letter:
-    print(alpha, " is a vowel")
-else:
-    print(alpha, " is not a vowel")
